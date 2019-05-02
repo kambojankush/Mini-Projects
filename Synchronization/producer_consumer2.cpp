92c7c1e4eb27946c0cfd948171bd6cc01191432f
Ankush <ankushkamboj06@gmail.com>
2019-05-02 18:50:23 +0530

    Producer consumer problem implementation with condition variables.

---
+++ Synchronization/producer_consumer2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+#include <thread>
+
+std::deque<int> q;
+std::mutex mu;
+std::condition_variable cond;
+
+void producer() {
+	int count = 10;
+	while (count > 0) {
+		std::unique_lock<std::mutex> locker(mu);
+		q.push_front(count);
+		locker.unlock();
+		cond.notify_one();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		count--;
+	}
+}
+
+// busy waiting
+void consumer() {
+	int data = 0;
+	while (data != 1) {
+		std::unique_lock<std::mutex> locker(mu);
+		cond.wait(locker); //spurious wake
+		 // cond.wait(locker, [](){return !q.empty();}); // pass predicate to check
+		data = q.back();
+		q.pop_back();
+		locker.unlock();
+		std::cout << "Consumer got a value from shared queue: " << data << std::endl;
+	}
+}
+
+int main() {
+	std::thread p(producer);
+	std::thread c(consumer);
+	p.join();
+	c.join();
+
+	return 0;
+}
