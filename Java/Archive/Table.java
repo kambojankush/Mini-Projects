a9d222fd4c1ff5b43ac2390116da43e209a93f63
Ankush <ankushkamboj06@gmail.com>
2019-06-25 17:59:45 +0530

    Do-While loop

---
+++ Java/Archive/Table.java
@@ -0,0 +1,14 @@
+public class Table {
+
+	public static void main(String[] args) {
+		int iVal = 5;
+
+		do {
+			System.out.print(iVal);
+			System.out.print(" * 2 = ");
+			iVal *= 2;
+			System.out.println(iVal);
+		}
+		while (iVal < 100);
+	}
+}
