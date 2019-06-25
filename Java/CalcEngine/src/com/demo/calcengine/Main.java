51791f75233b5322d7c1717835a3b9875e81e161
Ankush <ankushkamboj06@gmail.com>
2019-06-25 19:20:06 +0530

    Array+Switch Update

--- Java/CalcEngine/src/com/demo/calcengine/Main.java
+++ Java/CalcEngine/src/com/demo/calcengine/Main.java
@@ -3,24 +3,33 @@ package com.demo.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'a';
+        double[] leftVals = {100.0d, 90.0d, 120.0d, 11.0d};
+        double[] rightVals = {50.0d, 10.0d, 10.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if (opCode == 'a')
-            result = val1 + val2;
-        else if (opCode == 's')
-            result = val1 - val2;
-        else if (opCode == 'd')
-            result = (val2 == 0) ? 0.0d : val1/val2;
-        else if (opCode == 'm')
-            result = val1 * val2;
-        else {
-            System.out.println("Error: Invalid opCode");
-            result = 0.0d;
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm': 
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd': 
+                    results[i] = rightVals[i] == 0.0d ? 0.0d : leftVals[i] / rightVals[i];
+                    break;
+                default:
+                    results[i] = 0.0d;
+            }
         }
 
-        System.out.println(result);
+        for (double resultVal : results) {
+            System.out.print("result = ");
+            System.out.println(resultVal);
+        }
     }
 }
