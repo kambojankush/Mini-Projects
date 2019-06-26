59c39b5f9fcf04badc9baf2c71e31c5aa5a82bd9
Ankush <ankushkamboj06@gmail.com>
2019-06-26 18:55:18 +0530

    Field Encapsulation

--- Java/CalcEngine/src/com/demo/calcengine/MathEquation.java
+++ Java/CalcEngine/src/com/demo/calcengine/MathEquation.java
@@ -2,8 +2,17 @@ package com.demo.calcengine;
 
 public class MathEquation {
 
-	public double leftVal, rightVal, result;
-	public char opCode;
+	private double leftVal, rightVal, result;
+	private char opCode;
+
+	public double getLeftVal() {return leftVal;}
+	public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+	public double getRightVal() {return rightVal;}
+	public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+	public char getOpCode() {return opCode;}
+	public void setOpCode(char opCode) {this.opCode = opCode;}
+
+	public double getResult() {return result;}
 
 	public void execute() {
 		switch (opCode) {
