d37138845a72e4552f1759132cf347852457466f
Ankush <ankushkamboj06@gmail.com>
2019-06-28 20:07:36 +0530

    Output update

--- Java/CalcEngine/src/com/demo/calcengine/CalculateHelper.java
+++ Java/CalcEngine/src/com/demo/calcengine/CalculateHelper.java
@@ -2,6 +2,11 @@ package com.demo.calcengine;
 
 public class CalculateHelper {
 
+	private static final char ADD_SYMBOL = '+';
+	private static final char SUBTRACT_SYMBOL = '-';
+	private static final char DIVIDE_SYMBOL = '/';
+	private static final char MULTIPLY_SYMBOL = '*';
+
 	double leftVal, rightVal, result;
 	MathCommand command;
 
@@ -44,5 +49,36 @@ public class CalculateHelper {
 			command = MathCommand.Divide;
 		else if (commandString.equalsIgnoreCase(MathCommand.Multiply.toString()))
 			command = MathCommand.Multiply;
+	}
+
+	@Override
+	public String toString() {
+
+		char symbol = ' ';
+		switch (command) {
+			case Add:
+				symbol = ADD_SYMBOL;
+				break;
+			case Subtract:
+				symbol = SUBTRACT_SYMBOL;
+				break;
+			case Divide:
+				symbol = DIVIDE_SYMBOL;
+				break;
+			case Multiply:
+				symbol = MULTIPLY_SYMBOL;
+				break;
+		}
+
+		StringBuilder sb = new StringBuilder(20);
+		sb.append(leftVal);
+		sb.append(' ');
+		sb.append(symbol);
+		sb.append(' ');
+		sb.append(rightVal);
+		sb.append(" = ");
+		sb.append(result);
+
+		return sb.toString();
 	}
 }
