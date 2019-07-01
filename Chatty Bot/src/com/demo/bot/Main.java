b39f78ffd8904b2f872a76cc6253c1d28d6a1191
Ankush <ankushkamboj06@gmail.com>
2019-07-01 17:04:16 +0530

    Stage 1
    Signed-off-by: Ankush <ankushkamboj06@gmail.com>

---
+++ Chatty Bot/src/com/demo/bot/Main.java
@@ -0,0 +1,8 @@
+package com.demo.bot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello My name is Aid");
+        System.out.println("I was born in 2019.");
+    }
+}
